50717b19d897101c7d3c892faa0ea47aa0e31711
Evgen <sport777life@gmail.com>
2023-11-08 22:37:24 +0100

    + cookie

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, HttpException } from '@nestjs/common';
+import { Response } from 'express';
 import { UserService } from './user.service';
 import { LoginDto } from './dto/login.dto';
-import { IUserResponse } from './types/user.interface';
+import { IError } from './types/error.interface';
 
 @Controller()
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
     @Post('login')
-    async login(@Body() body: LoginDto): Promise<IUserResponse> {
-        const { username } = body
-        return this.userService.login(username);
+    async login(@Body() body: LoginDto, @Res({ passthrough: true }) res: Response): Promise<HttpStatus | IError> {
+        try {
+            const { username } = body
+            const user = await this.userService.login(username);
+            res.cookie('user', user, { httpOnly: true, secure: true })
+            return HttpStatus.OK;
+        } catch (err) {
+            res.status(400)
+            return { error: true, message: err.message }
+        }
     }
 }
